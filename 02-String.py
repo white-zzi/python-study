b98fdf7a354695e3c8790df7d7713576d946afe0
김희진 <lovesky3265@gmail.com>
2022-09-11 00:24:22 +0900

    init

---
+++ 02-String.py
@@ -0,0 +1,48 @@
+a = 'chan oug unday'
+b = '히찌꺼'
+print (a+b)
+
+# multistring.py
+
+print("=" * 50)
+print("My Program")
+print("=" * 50)
+
+a = "heezzi is chanoug zzizzi very want"
+print (len(a))
+
+a = "heezzi is chanoug zzizzi very want"
+b = a[0] + a[2] + a[7] + a[8]
+print(b)
+
+a = "heezzi is chanoug zzizzi very want"
+print(a[0:6])
+
+a = "heezzi is chanoug zzizzi very want"
+print(a[10:])
+
+print(a[:])
+
+print(a[9:-10])
+
+a = "Chanung is HeeZzi zzizzi very want"
+print(a)
+
+a = "heezzi is chanuug zzizzi very want"
+print(a)
+
+a = "20010331rainy"
+date = a[:8]
+weather = a[8:]
+print(date)
+print(weather)
+
+a = "20010331rainy"
+year = a[:4]
+day = a[4:8]
+weather = a [8:]
+print(year)
+print(day)
+print(weather)
+
+# ["Pithon"이라는 문자열을 "Python"으로 바꾸려면?] keep
